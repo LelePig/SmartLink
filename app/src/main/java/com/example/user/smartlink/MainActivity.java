3a55a9a391d357338764ea5879c7f95981845a24
LelePig <fangsunlight@163.com>
2017-06-12 11:04:02 +0800

    去掉测试日志

--- app/src/main/java/com/example/user/smartlink/MainActivity.java
+++ app/src/main/java/com/example/user/smartlink/MainActivity.java
@@ -46,7 +46,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         elianNative=new ElianNative();
         boolean isLoad=elianNative.LoadLib();
         regFilter();
-        Log.e("leleTest","leleTest-------");
 
     }
     public void regFilter(){
